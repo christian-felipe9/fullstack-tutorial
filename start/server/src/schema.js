2412f938cf6afe98c127c397725286e1e230de0f
Christian Felipe Lopes Dias <Christian_Felipe9@hotmail.com>
2020-02-11 21:53:49 -0300

    Initial Commit

---
+++ start/server/src/schema.js
@@ -0,0 +1,13 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Launch {
+  id: ID!
+  site: String
+  mission: Mission
+  rocket: Rocket
+  isBooked: Boolean!
+}
+`;
+
+module.exports = typeDefs;
