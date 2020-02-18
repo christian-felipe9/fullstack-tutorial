2c51a66b49bfc83b7ad6f904301e17d4746ab829
Christian Felipe Lopes Dias <Christian_Felipe9@hotmail.com>
2020-02-17 21:08:18 -0300

    Server Almost Finished

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,13 +1,68 @@
-const { gql } = require('apollo-server');
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   type Launch {
-  id: ID!
-  site: String
-  mission: Mission
-  rocket: Rocket
-  isBooked: Boolean!
-}
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+  }
+
+  type Mission {
+    name: String
+    missionPatch(mission: String, size: PatchSize): String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+
+  type LaunchConnection {
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
+  }
+
+  type Query {
+    launches(
+      """
+      The number of results to show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+      """
+      If you add a cursor here, it will only return results _after_ this cursor
+      """
+      after: String
+    ): LaunchConnection!
+    launch(id: ID!): Launch
+    me: User
+  }
+
+  type Mutation {
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+    login(email: String): String # login token
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
+  }
 `;
 
 module.exports = typeDefs;
